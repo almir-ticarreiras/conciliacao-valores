41fa277c152a854371e07303ad2e6b0623b94d93
Almir Ferreira <ticarreiras@gmail.com>
2024-11-23 22:59:23 -0300

    tratamento de excel do balancete do SIGA - CCB

---
+++ trata_pdf.py
@@ -0,0 +1,7 @@
+import PyPDF2
+
+caminho_arquivo_pdf = "file_base/arquivo1.pdf"
+valor_procurado = "CNPJ"
+
+print("funcionando até aqui!")
+print("irei procurar no arquivo: ", caminho_arquivo_pdf, " o valor: ", valor_procurado)
